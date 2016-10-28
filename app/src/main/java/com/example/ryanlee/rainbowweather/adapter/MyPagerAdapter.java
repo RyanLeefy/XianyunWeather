b68d24122007192eee04cc52cbcde368d21c7da4
ryanleefy <ryanleefuyu@gmail.com>
2016-10-29 01:16:49 +0800

    修改了UI，添加了折线图，竖直Vierpager

---
+++ app/src/main/java/com/example/ryanlee/rainbowweather/adapter/MyPagerAdapter.java
@@ -0,0 +1,7 @@
+package com.example.ryanlee.rainbowweather.adapter;
+
+/**
+ * Created by Ryanlee on 2016/10/28 0028.
+ */
+public class MyPagerAdapter {
+}
---
+++ app/src/main/java/com/example/ryanlee/rainbowweather/bean/Temperatur.java
@@ -0,0 +1,28 @@
+package com.example.ryanlee.rainbowweather.bean;
+
+/**
+ * Created by Ryanlee on 2016/10/28 0028.
+ */
+public class Temperatur {
+
+    private String minTmp;
+    private String maxTmp;
+
+    public String getMinTmp() {
+        return minTmp;
+    }
+
+    public void setMinTmp(String minTmp) {
+        this.minTmp = minTmp;
+    }
+
+    public String getMaxTmp() {
+        return maxTmp;
+    }
+
+    public void setMaxTmp(String maxTmp) {
+        this.maxTmp = maxTmp;
+    }
+
+
+}
